093f9279efa60426445d3029226519806ea21b02
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-04-30 14:47:06 -0300

    add material ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
